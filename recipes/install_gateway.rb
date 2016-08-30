59d142338e57b4c99cb6063642f09ec2676ce86e
Marcelo Mandolesi <mmandolesi@gannett.com>
2016-08-30 16:22:25 -0400

    Adding testing and updating file limits

--- recipes/install_gateway.rb
+++ recipes/install_gateway.rb
@@ -9,7 +9,13 @@ include_recipe 'sysctl::apply'
 limits_config 'system limits' do
   limits [
     { domain: '*', type: 'hard', item: 'nofile', value: '65536' },
-    { domain: '*', type: 'soft', item: 'nofile', value: '65536' }
+    { domain: '*', type: 'soft', item: 'nofile', value: '65536' },
+    { domain: '*', type: 'hard', item: 'nproc', value: '65536' },
+    { domain: '*', type: 'soft', item: 'nproc', value: '65536' },
+    { domain: 'root', type: 'hard', item: 'nofile', value: '65536' },
+    { domain: 'root', type: 'soft', item: 'nofile', value: '65536' },
+    { domain: 'root', type: 'hard', item: 'nproc', value: '65536' },
+    { domain: 'root', type: 'soft', item: 'nproc', value: '65536' }
   ]
   use_system true
 end
